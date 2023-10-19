18795b8024e2d936f33ca04403662e55c9ae454a
bblackskulll063 <98683404+bblackskulll063@users.noreply.github.com>
2023-10-20 00:23:21 +0530

    Revert "perfect prject"
    This reverts commit 6e9d8856c726dea7b06ab2cd5cbce0b4e1dd380a.

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-require('dotenv').config()
+// require('dotenv').config()
 // const url = process.env.MONGO_ATLAS_URL;
 const url = "mongodb://127.0.0.1/crudUser";
-// const url = "https://crudcrud.com/api/c9a0784432a7427bbda1dc1e7a8695bc";
 
 const connectToMongo = async ()=>{
     try {
